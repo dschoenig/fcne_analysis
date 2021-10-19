d47da88d8bded696121d73f9c02277371912fecc
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-10-19 13:08:24 -0400

    Correct script reference

--- src/test_eval_post_array_cam.sh
+++ src/test_eval_post_array_cam.sh
@@ -8,4 +8,4 @@
 
 module load StdEnv/2020 r/4.1.0
 
-Rscript 4a_evaluate_posterior_mar_cam.R $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
+Rscript 4d_evaluate_posterior_mar_cam.R $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
