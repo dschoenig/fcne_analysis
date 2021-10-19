bc433707ad8e4ad880e4239b07126762528a2167
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-10-19 16:19:09 -0400

    Increase time for array job

--- src/cc_evaluate_posterior_mar_amz.sh
+++ src/cc_evaluate_posterior_mar_amz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --mem-per-cpu=20G
-#SBATCH --time=04:00:00
+#SBATCH --time=05:30:00
 #SBATCH --array=1-100
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
