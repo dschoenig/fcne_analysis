c53f10c24e3c70b30e4bb73bab2ee308eb7d649e
Daniel Schoenig <schoed@beluga1.int.ets1.calculquebec.ca>
2021-10-27 15:00:42 -0400

    Compare effect of dropping intercept

---
+++ src/test_eval_post2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-cricrime 
+#SBATCH --mem-per-cpu=36G
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020 r/4.1.0
+
+Rscript 4_evaluate_lp.R CAM marginal 95 200
--- src/test_model_intercept_amz.sh
+++ src/test_model_intercept_amz.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=96G
-#SBATCH --time=10:00:00
+#SBATCH --mem=80G
+#SBATCH --time=05:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
 module load StdEnv/2020 r/4.1.0
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-Rscript test_model_intercept_cam.R CAM int $SLURM_CPUS_PER_TASK
-Rscript test_model_intercept_cam.R CAM drop $SLURM_CPUS_PER_TASK
+Rscript test_model_intercept_amz.R AMZ int $SLURM_CPUS_PER_TASK
+Rscript test_model_intercept_amz.R AMZ drop $SLURM_CPUS_PER_TASK
