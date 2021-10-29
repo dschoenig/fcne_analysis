15fee9fa397ea64bb0a83ee401b9bc35e77165f1
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-10-29 16:33:37 -0400

    Batch scripts for improved hierarchical structure

---
+++ src/cc_fit_model_AMZ_4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=def-cricrime 
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --time=03:00:00
+#SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020 r/4.1.0
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+Rscript 2_fit_models.R AMZ 4 $SLURM_CPUS_PER_TASK
+
