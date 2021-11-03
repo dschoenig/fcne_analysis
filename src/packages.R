3a060cdd825b4a9e2dba9cfec9f949a28251aaee
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-11-03 11:31:31 -0400

    Update package requirements

--- src/packages.R
+++ src/packages.R
@@ -1,3 +1,3 @@
-install.packages(c("mgcv", "data.table", "kohonen",
+install.packages(c("mgcv", "data.table", "kohonen", "doParallel",
                    "mvnfast", "posterior", "arrow", "dplyr",
                    "bayesplot", "ggplot2", "ggdist", "patchwork")
