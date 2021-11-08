1fa0f8b03da0a8150cc4c01368cee2f19d9412bd
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-11-08 15:53:30 -0500

    Sync project and scratch folders

---
+++ src/cc_sync_project_to_scratch.sh
@@ -0,0 +1,2 @@
+rsync -rltv  /home/schoed/projects/def-cricrime/schoed/fcne_analysis/models /home/schoed/scratch/fcne_analysis/models
+rsync -rltv /home/schoed/projects/def-cricrime/schoed/fcne_analysis/data /home/schoed/scratch/fcne_analysis/data 
