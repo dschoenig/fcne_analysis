25b4c5f24e658a185d24e7816cc0aceac7959ad6
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-11-11 11:13:11 -0500

    Correct typo (again)

--- src/cc_convert_arrow_AMZ.sh
+++ src/cc_convert_arrow_AMZ.sh
@@ -15,5 +15,5 @@ DIR_LP="/home/schoed/scratch/fcne_analysis/models/gam/lp/amz.lp"
 rm -rf ${DIR_LP}
 mv ${DIR_LP}.arrow ${DIR_LP}
 # Rename for CentOS
-rename --verbose amz.lp- amz.lp-00 ${DIR_LP}*/amz.lp-?.arrow
-rename --verbose amz.lp- amz.lp-0 ${DIR_LP}*/amz.lp-??.arrow
+rename --verbose amz.lp- amz.lp-00 ${DIR_LP}/*/amz.lp-?.arrow
+rename --verbose amz.lp- amz.lp-0 ${DIR_LP}/*/amz.lp-??.arrow
