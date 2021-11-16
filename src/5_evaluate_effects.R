ada381d59dcc5e9b094f8012f9b73e471023a80b
Daniel Schoenig <schoed@narval1.narval.calcul.quebec>
2021-11-16 13:51:00 -0500

    Resource use (eval effects)

--- src/5_evaluate_effects.R
+++ src/5_evaluate_effects.R
@@ -59,8 +59,7 @@ for(i in seq_along(marginals)) {
   effects[[i]] <- summarize_predictions(ds,
                                    ids = id.list,
                                    draw.ids = as.character(1:1000),
-                                   # draw.chunk = 100,
-                                   draw.chunk = 1000,
+                                   draw.chunk = 100,
                                    clamp = link_cll(c(.Machine$double.eps, 1-.Machine$double.eps)),
                                    n.threads = n.threads
                                    )
