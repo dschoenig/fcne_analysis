712987419e803bcdf8bce3d8800202022d85dd7c
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-12-09 14:51:11 -0500

    Larger sample size

--- src/0_data_preprocessing.R
+++ src/0_data_preprocessing.R
@@ -4,11 +4,16 @@ library(data.table)
 path.raw <- "../data/raw/"
 path.int <- "../data/intermediate/"
 
+n.fit <- 1e7
+n.val <- 2e6
+
 regions <- c("amz", "cam")
+
 
 for(i in 1:length(regions)) {
   file.data.raw <- paste0(path.raw, regions[i], ".vars.csv")
-  file.data.int <- paste0(path.int, regions[i], ".data.int.rds")
+  file.data.fit.int <- paste0(path.int, regions[i], ".data.fit.int.rds")
+  file.data.val.int <- paste0(path.int, regions[i], ".data.val.int.rds")
 
   vars <- fread(file.data.raw, 
                     na.strings = "",
@@ -40,8 +45,19 @@ for(i in 1:length(regions)) {
                                           "not_recognized:direct_use"),
                                ordered = TRUE)]
 
-  data.int <- na.omit(vars, cols = c("dist_set", "dist_roads", "dist_rivers", "slope", "adm0"))
-  saveRDS(data.int, file.data.int)
-  rm(data.int, vars)
-}
+  # Remove samples with incomplete covariate information and cut sample to
+  # 12 million
+  data.int <- na.omit(vars)[1:(n.fit + n.val),]
 
+  # Randomly assign samples to fit and validation set
+  set.seed(18470611)
+  sam <- sample(1:nrow(data.int), nrow(data.int), replace = FALSE)
+  data.fit.int <- data.int[sam[1:n.fit],]
+  data.val.int <- data.int[sam[(n.fit+1):(n.fit + n.val)],]
+
+  length(which(data.val.int$id %in% data.fit.int$id)) == 0
+
+  saveRDS(data.fit.int, file.data.fit.int)
+  saveRDS(data.val.int, file.data.val.int)
+  rm(data.int, data.fit.int, data.fit.val, vars)
+}
