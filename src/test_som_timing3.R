72244173be6404c6a0a3d06b55a30b9e248a5349
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-12-10 17:54:51 -0500

    More 16 core tests

--- src/test_som_timing3.R
+++ src/test_som_timing3.R
@@ -39,6 +39,23 @@ grid <- somgrid(xdim = xdim, ydim = ydim,
 som.init <- init_som(train.1e4, xdim, ydim)
 
 
+message("Small, 10 epochs, 16 cores")
+a <- Sys.time()
+som(train.1e6, grid = grid, 
+    rlen = 10, mode = "pbatch", 
+    init = som.init, cores = 16,
+    normalizeDataLayers = FALSE)
+b <- Sys.time()
+print(b-a)
+
+message("Medium, 10 epochs, 16 cores")
+a <- Sys.time()
+som(train.1e6, grid = grid, 
+    rlen = 10, mode = "pbatch", 
+    init = som.init, cores = 16,
+    normalizeDataLayers = FALSE)
+b <- Sys.time()
+print(b-a)
 
 message("Large, 10 epochs, 16 cores")
 a <- Sys.time()
