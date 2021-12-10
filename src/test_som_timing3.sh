89818025ee54ff0866276db85fde9ccc8fd9121b
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-12-10 14:45:22 -0500

    More timing for 16 cores

--- src/test_som_timing3.sh
+++ src/test_som_timing3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=12G
+#SBATCH --mem=8G
 #SBATCH --time=00:15:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
@@ -9,4 +9,4 @@
 module load StdEnv/2020 r/4.1.0
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-Rscript test_som_timing2.R
+Rscript test_som_timing3.R
