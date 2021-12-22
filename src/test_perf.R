bc3e773cc8428b2606140a71f87acd3984c430b3
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-12-22 11:25:40 -0500

    2e6 full size

--- src/test_perf.R
+++ src/test_perf.R
@@ -361,6 +361,52 @@ if(model.id == "t6") {
         )
 }
 
+
+if(model.id == "t7") {
+  
+  # Model size
+  k.def <- c(ten_loc.bl = 750,
+             ten_loc.itpa = 500,
+             ten_loc.ov = 250,
+             som = 750)
+  max.knots.def <- c(k.def[1:3] * 10, som = 10000)
+  data.proc <- data.proc[1:2e6,]
+  
+  data.mod <- 
+    as.data.frame(data.proc[, .(forestloss,
+                            it_type, pa_type, overlap, ed_east, ed_north,
+                            adm0, som_x, som_y)])
+  data.mod$b0 <- model.matrix(~ 1, data.mod)
+  rm(data.proc)
+
+  model <-
+    bam(forestloss ~
+        -1 + b0 +
+        s(ed_east, ed_north, bs = 'gp',
+          k = k.def["ten_loc.bl"],
+          xt = list(max.knots = max.knots.def["ten_loc.bl"])) +
+        s(ed_east, ed_north, bs = 'gp',
+          by = it_type, k = k.def["ten_loc.itpa"],
+          xt = list(max.knots = max.knots.def["ten_loc.itpa"])) +
+        s(ed_east, ed_north, bs = 'gp',
+          by = pa_type, k = k.def["ten_loc.itpa"],
+          xt = list(max.knots = max.knots.def["ten_loc.itpa"])) +
+        s(ed_east, ed_north, bs = 'gp',
+          by = overlap, k = k.def["ten_loc.ov"],
+          xt = list(max.knots = max.knots.def["ten_loc.ov"])) +
+        s(som_x, som_y, bs = 'gp',
+          by = adm0, k = k.def["som"], xt = list(max.knots = max.knots.def["som"])),
+        family = binomial(link = "cauchit"),
+        data = data.mod,
+        select = TRUE,
+        paraPen = list(b0 = list(diag(1))),
+        chunk.size = 1e5,
+        discrete = TRUE,
+        nthreads = n.threads,
+        gc.level = 0
+        )
+}
+
 warnings()
 
 print("Saving fitted model …")
