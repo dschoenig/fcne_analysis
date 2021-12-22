0295b141724ef952c96689b59b150b4600b200f3
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-12-22 12:25:20 -0500

    Test perf 3e6

--- src/test_perf8.sh
+++ src/test_perf8.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
+#SBATCH --mem=52G
 #SBATCH --time=02:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
 module load StdEnv/2020 r/4.1.2
 export OMP_NUM_THREADS=$SLRUM_CPUS_PER_TASK
-Rscript test_perf.R CAM t7 $SLURM_CPUS_PER_TASK
+Rscript test_perf.R CAM t8 $SLURM_CPUS_PER_TASK
