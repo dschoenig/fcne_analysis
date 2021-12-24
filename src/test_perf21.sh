de410fb392bc96371cab446d8d9f8fd8caf8d022
Daniel Schoenig <daniel.schoenig@protonmail.com>
2021-12-24 09:38:08 -0500

    Cauchit model, resource allocation

--- src/test_perf21.sh
+++ src/test_perf21.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=42G
+#SBATCH --mem=80G
 #SBATCH --time=08:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
