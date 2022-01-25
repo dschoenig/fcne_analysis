da00b5778a4740ffb4c75d6761fddd3e72909636
Daniel Schoenig <daniel.schoenig@protonmail.com>
2022-01-25 11:39:05 -0500

    typo...

--- src/cc_evaluate_effects_location_CAM.sh
+++ src/cc_evaluate_effects_location_CAM.sh
@@ -9,4 +9,4 @@
 module load StdEnv/2020 r/4.1.2
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-Rscript 5b_evaluate_effects_tenure.R CAM $SLURM_CPUS_PER_TASK
+Rscript 5b_evaluate_effects_location.R CAM $SLURM_CPUS_PER_TASK
