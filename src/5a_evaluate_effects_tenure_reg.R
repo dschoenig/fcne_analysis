d577ca98441b85dc948cc4c95306728e55c6c58f
Daniel Schoenig <daniel.schoenig@protonmail.com>
2022-01-31 07:23:24 -0500

    typo

--- src/5a_evaluate_effects_tenure_reg.R
+++ src/5a_evaluate_effects_tenure_reg.R
@@ -8,7 +8,7 @@ n.threads <- as.integer(args[2])
 # n.threads <- 1
 
 path.base <- "/home/schoed/scratch/fcne_analysis/"
-path.base <- "../"
+# path.base <- "../"
 path.lp <- paste0(path.base, "models/gam/lp/")
 path.data.proc <- paste0(path.base, "data/processed/")
 path.effects <- paste0(path.base, "models/gam/effects/")
