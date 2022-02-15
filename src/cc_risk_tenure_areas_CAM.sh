8404f1facfaf1f9d89fb8b9a2cb52860a9924358
Daniel Schoenig <daniel.schoenig@protonmail.com>
2022-02-15 13:34:47 -0500

    Risk: resource allocation

--- src/cc_risk_tenure_areas_CAM.sh
+++ src/cc_risk_tenure_areas_CAM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=112G
-#SBATCH --time=15:00:00
+#SBATCH --mem=96G
+#SBATCH --time=8:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
