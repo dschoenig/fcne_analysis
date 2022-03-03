ecdc785be48ce5ab752b2e277af681b12122af95
Daniel Schoenig <daniel.schoenig@protonmail.com>
2022-03-02 19:11:31 -0500

    SOM fit, bump R version

--- src/cc_fit_som_AMZ.sh
+++ src/cc_fit_som_AMZ.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
-module load StdEnv/2020 r/4.1.0
+module load StdEnv/2020 r/4.1.2
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 Rscript 1a_fit_som.R AMZ $SLURM_CPUS_PER_TASK
