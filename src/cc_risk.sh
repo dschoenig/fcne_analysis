e75931029e694bee42d318f97ddf8249ac65b731
Daniel Schoenig <daniel.schoenig@protonmail.com>
2022-03-15 11:31:31 -0400

    Rename

--- src/cc_risk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sbatch cc_risk_geo_all_AMZ.sh
-sbatch cc_risk_geo_it_AMZ.sh
-sbatch cc_risk_geo_pa_AMZ.sh
-sbatch cc_risk_tenure_AMZ.sh
-sbatch cc_risk_tenure_areas_AMZ.sh
-sbatch cc_risk_tenure_cov_AMZ.sh
