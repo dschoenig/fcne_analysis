eae664842601bcc2c15d3b0264eee7d54b21742c
Daniel Schoenig <schoed@gra-login1.graham.sharcnet>
2022-03-16 09:50:51 -0400

    Resource allocation for risk eval

--- src/cc_risk_geo_it_AMZ.sh
+++ src/cc_risk_geo_it_AMZ.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=96G
-#SBATCH --time=14:00:00
+#SBATCH --mem=112G
+#SBATCH --time=20:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
