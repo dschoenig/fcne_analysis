c41fb86cfc03e3d33a16afc42ab39a499b55b262
Daniel Schoenig <schoed@narval3.narval.calcul.quebec>
2022-03-24 14:36:24 -0400

    Risk eval: resource allocation

--- src/cc_risk_tenure_cov_CAM.sh
+++ src/cc_risk_tenure_cov_CAM.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=112G
-#SBATCH --time=10:00:00
+#SBATCH --time=14:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
