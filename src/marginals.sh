c851d27ac858b94dd0a87505f7ad7a22c95e00e2
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-03-11 15:33:41 -0400

    Marginals: CAM without Otto

--- src/marginals.sh
+++ src/marginals.sh
@@ -1,12 +1,38 @@
-sbatch cc_6_marginal_geo_it_AMZ.sh
-sbatch cc_6_marginal_geo_itpa_AMZ.sh
-sbatch cc_6_marginal_geo_pa_AMZ.sh
-sbatch cc_6_marginal_ten_areas_full_all_AMZ.sh
-sbatch cc_6_marginal_ten_areas_full_ov_AMZ.sh
-sbatch cc_6_marginal_ten_areas_it_AMZ.sh
-sbatch cc_6_marginal_ten_areas_pa_AMZ.sh
+# sbatch cc_6_marginal_geo_it_AMZ.sh
+# sbatch cc_6_marginal_geo_itpa_AMZ.sh
+# sbatch cc_6_marginal_geo_pa_AMZ.sh
+# sbatch cc_6_marginal_ten_areas_full_all_AMZ.sh
+# sbatch cc_6_marginal_ten_areas_full_ov_AMZ.sh
+# sbatch cc_6_marginal_ten_areas_it_AMZ.sh
+# sbatch cc_6_marginal_ten_areas_pa_AMZ.sh
 # sbatch cc_6_marginal_ten_it_all_AMZ.sh
 # sbatch cc_6_marginal_ten_it_ov_AMZ.sh
-sbatch cc_6_marginal_ten_itpa_AMZ.sh
+# sbatch cc_6_marginal_ten_itpa_AMZ.sh
 # sbatch cc_6_marginal_ten_pa_all_AMZ.sh
 # sbatch cc_6_marginal_ten_pa_ov_AMZ.sh
+
+# sbatch cc_6_marginal_geo_it_CAM.sh
+# sbatch cc_6_marginal_geo_itpa_CAM.sh
+# sbatch cc_6_marginal_geo_pa_CAM.sh
+# sbatch cc_6_marginal_ten_areas_full_all_CAM.sh
+# sbatch cc_6_marginal_ten_areas_full_ov_CAM.sh
+# sbatch cc_6_marginal_ten_areas_it_CAM.sh
+# sbatch cc_6_marginal_ten_areas_pa_CAM.sh
+# sbatch cc_6_marginal_ten_it_all_CAM.sh
+# sbatch cc_6_marginal_ten_it_ov_CAM.sh
+# sbatch cc_6_marginal_ten_itpa_CAM.sh
+# sbatch cc_6_marginal_ten_pa_all_CAM.sh
+# sbatch cc_6_marginal_ten_pa_ov_CAM.sh
+
+sbatch cc_6_marginal_geo_it_no_otto_CAM.sh
+sbatch cc_6_marginal_geo_itpa_no_otto_CAM.sh
+sbatch cc_6_marginal_geo_pa_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_areas_full_all_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_areas_full_ov_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_areas_it_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_areas_pa_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_it_all_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_it_ov_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_itpa_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_pa_all_no_otto_CAM.sh
+sbatch cc_6_marginal_ten_pa_ov_no_otto_CAM.sh
