c51ed2776b4bdcb4947fa87530da839a525a4f0a
Daniel Schoenig <schoed@narval3.narval.calcul.quebec>
2024-08-01 09:39:04 -0400

    Cluster, counterfactual and marginal: increase resource allocation

--- src/cc_5_counterfactual_ten_AMZ.sh
+++ src/cc_5_counterfactual_ten_AMZ.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=48G
+#SBATCH --mem=72G
 #SBATCH --time=10:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
