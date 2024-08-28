fcb8339122a758aa9c008b7c59b504f18081ad53
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-28 11:27:01 -0400

    Residuals: fix typo cluster

--- src/cc_6_residuals_CAM.sh
+++ src/cc_6_residuals_CAM.sh
@@ -12,4 +12,4 @@ module load StdEnv/2023 gcc/12.3 gdal/3.7.2 geos/3.12.0 python/3.11.5 udunits/2.
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 Rscript 6_residuals.R cam def
-Rscript 6_residuals.R cam def
+Rscript 6_residuals.R cam deg
